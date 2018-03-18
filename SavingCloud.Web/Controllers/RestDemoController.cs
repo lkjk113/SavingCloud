d3adda3e74ae961e7081346be551a6c48faa60ea
JohnYue <yh113@sina.com>
2018-03-18 23:27:53 +0800

    添加Swagger

--- SavingCloud.Web/Controllers/RestDemoController.cs
+++ SavingCloud.Web/Controllers/RestDemoController.cs
@@ -7,31 +7,56 @@ using System.Web.Http;
 
 namespace SavingCloud.Controllers
 {
-    public class ValuesController : ApiController
+    /// <summary>
+    /// Restful接口示例
+    /// </summary>
+    public class RestDemoController : ApiController
     {
         // GET api/values
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
+        /// <summary>
+        /// 获取by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public string Get(int id)
         {
             return "value";
         }
 
         // POST api/values
+        /// <summary>
+        /// 创建
+        /// </summary>
+        /// <param name="value"></param>
         public void Post([FromBody]string value)
         {
         }
 
         // PUT api/values/5
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
         public void Put(int id, [FromBody]string value)
         {
         }
 
         // DELETE api/values/5
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id"></param>
         public void Delete(int id)
         {
         }
